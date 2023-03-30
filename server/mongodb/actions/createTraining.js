b9bf4907a5ecd19e938a1aa704379ad3630fbdcb
zchakraborty <zinichakraborty@gmail.com>
2023-03-30 19:26:17 -0400

    add: compare userid with animal's ownerid

--- server/mongodb/actions/createTraining.js
+++ server/mongodb/actions/createTraining.js
@@ -3,10 +3,14 @@ import Training from "../models/training.js"
 
 export default async function createTraining(trainingData) {
     await connectDB()
-    try {
-        const training = new Training(trainingData)
-        await training.save()
-    } catch (e) {
-        throw new Error("Unable to create training log. Invalid data")
+    const training = new Training(trainingData)
+    if (training.user._id == training.animal.owner._id) {
+        try {
+            await training.save()
+        } catch (e) {
+            return res.status(400).send("Unable to create training log. Invalid data")
+        }
+    } else {
+        return res.status(400).send("Unable to create training log. Invalid data")
     }
 }
