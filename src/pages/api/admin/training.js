2449a823d16fb9de6a0b001535ffc8f80bce83ad
Tanya Qin <tanyaqin21@gmail.com>
2023-04-03 19:46:09 -0400

    Finish admin/training GET endpoint to return a page of training logs and add admin/user GET endpoint without not returning password functionality

--- src/pages/api/admin/training.js
+++ src/pages/api/admin/training.js
@@ -3,11 +3,11 @@ import getTraining from "../../../server/mongodb/actions/getTraining";
 export default async function handler(req, res) {
     if (req.method == 'GET') {
         try {
-            await getTraining(req.body)
+            await getTraining(req)
         } catch (e) {
-            return res.status(500).send("Unable to get all training logs")
+            return res.status(500).send("Unable to get training logs")
         }
-        return res.status(200).send("Successfully returned all training logs")
+        return res.status(200).send("Successfully returned training logs")
     }
     return res.status(400).send("Incorrect req method type")
 
