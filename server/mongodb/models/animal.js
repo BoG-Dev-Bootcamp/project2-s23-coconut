043edffc3b5dbdb7c743ce945be59d54c7712861
zchakraborty <zinichakraborty@gmail.com>
2023-04-04 19:28:49 -0400

    fix: schema keys

--- server/mongodb/models/animal.js
+++ server/mongodb/models/animal.js
@@ -9,7 +9,7 @@ const animalSchema = new mongoose.Schema({
         type: Number,
         required: true
     },
-    owner: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     dateOfBirth: {
         type: Date,
         required: true
