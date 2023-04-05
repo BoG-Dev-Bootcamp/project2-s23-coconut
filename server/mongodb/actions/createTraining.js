0ef9f225a33811191cfff4d8eb6fa77e7a0055ba
zchakraborty <zinichakraborty@gmail.com>
2023-04-05 19:23:08 -0400

    add: create training extra endpoints

--- server/mongodb/actions/createTraining.js
+++ server/mongodb/actions/createTraining.js
@@ -1,10 +1,25 @@
 import connectDB from "../index.js"
 import Training from "../models/training.js"
+import getAnimalById from "./getAnimalById.js"
+import getUserById from "./getUserById.js"
 
 export default async function createTraining(trainingData) {
     await connectDB()
     const training = new Training(trainingData)
-    if (training.user._id == training.animal.owner._id) {
+    let animal = training.animal
+    try {
+        animal = await getAnimalById(animal)
+    } catch (e) {
+        return res.status(400).send("Unable to find animal")
+    }
+    let user
+    try {
+        user = await getUserById(animal.owner)
+    } catch (e) {
+        return res.status(400).send("Unable to find animal")
+    }
+    console.log(training.user)
+    if (training.user == user) {
         try {
             await training.save()
         } catch (e) {
