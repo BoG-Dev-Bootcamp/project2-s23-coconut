08f20bf8abdcf085d151d2aafbeddbf03dd0dffd
Tanya Qin <tanyaqin21@gmail.com>
2023-04-06 19:32:21 -0400

    Created verify endpoint with res statuses but no implementation

--- src/pages/api/user/verify.js
+++ src/pages/api/user/verify.js
@@ -1,12 +1,21 @@
 import auth from "../../../../server/utils/auth"
 
-export default function handler(req, res) {
-    try {
-        // if user email + password correct logic
-        const decoded = auth(req)
-        return res.status(200).json(decoded)
-    }
-    catch (e) {
-        return res.status(400).send(e.message)
+export default async function handler(req, res) {
+    if (req.method == 'POST') {
+        try {
+            const token = sign({ admin: true }, process.env.SECRET, { expiresIn: '60s' })
+    
+            const serialized = serialize("OurJWT", token, {
+                httpOnly: true,
+                secure: false, // change this in production
+                sameSite: "strict",
+                maxAge: 60,
+            });
+            res.setHeader('Set-Cookie', serialized)
+            res.status(200).send("JWT Created! " + token)
+        }
+        catch (e) {
+            return res.status(400).send(e.message)
+        }
     }
 }
