b074e1be84b43e685031a25900f0309f87be5330
zchakraborty <zinichakraborty@gmail.com>
2023-04-06 00:45:13 -0400

    add: bcrypt and hash

--- server/mongodb/actions/createUser.js
+++ server/mongodb/actions/createUser.js
@@ -1,12 +1,17 @@
 import connectDB from "../index.js"
 import User from "../models/user.js"
+import bcrypt from "bcryptjs"
 
 export default async function createUser(userData) {
     await connectDB()
     try {
-        const user = new User(userData)
+        const { firstName, lastName, email, password } = userData
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(password, salt)
+        const user = new User({firstName, lastName, email, password : hash})
         await user.save()
     } catch (e) {
+        console.log(e)
         throw new Error("Unable to create user. Invalid data")
     }
 }
